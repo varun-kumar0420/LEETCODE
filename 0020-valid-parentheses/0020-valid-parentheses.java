03102aec449a1417b0904def6a430a0649daa29c
Varun Kumar <varun8511280@gmail.com>
2026-08-10 00:26:09 +0530

    Time: 4 ms (36.69%), Space: 43.2 MB (58.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s==null || s.length()%2!=0) return false;
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+
+                char top = st.pop();
+                if((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
